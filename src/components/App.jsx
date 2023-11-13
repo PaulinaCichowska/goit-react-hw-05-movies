aa6bcfa619f140ae0165fa2c684143776c98b318
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-11-13 14:47:29 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
     <>
+    <Routes>
+    <Route path="/" element={<CustomeNavigation />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies/>} />
+        <Route path="/movies/:movieId" element={<MovieDetails/>} />
+        <Route path="/movies/:movieId/cast" element={<Cast/>} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews/>} />
+      </Route>
+    </Routes>
     </>
   );
 };
